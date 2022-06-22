86f6cdd7ec2ee95d6f2303cd4fbe33a7a0b4ccd5
Cristian Martinez <cdturizo@uninorte.edu.co>
2022-06-22 09:58:25 -0500

    fixed bug

--- to_do_list/index.js
+++ to_do_list/index.js
@@ -1,6 +1,7 @@
 var okbtn = document.getElementById("add-btn");
 var inputText = document.getElementById("input-todo");
 var list = document.getElementById("list-container");
+var trash
 okbtn.addEventListener("click", addToList );
 inputText.addEventListener("keyup", function(e){
     if(e.key == "Enter"){
@@ -14,9 +15,9 @@ function addToList(){
         alert("Accion no puede estar vacia")
     }
     else{
-        var action = document.createTextNode(inputText.value); 
-        myAction = document.createElement('li')
-        deletebtn = document.createElement('span')
+        let action = document.createTextNode(inputText.value); 
+        let myAction = document.createElement('li')
+        let deletebtn = document.createElement('span')
         deletebtn.innerHTML = "delete"
         deletebtn.id = "delete" + (list.childElementCount + 1);
         myActionContainer = document.createElement('div')
@@ -29,23 +30,12 @@ function addToList(){
         myActionContainer.id = "container" + (list.childElementCount + 1)
         list.appendChild(myActionContainer)
         inputText.value = ""
-        trash = list.querySelectorAll("span")
-        trash.forEach(element => {
-            element.addEventListener("click",()=>{
-                try {
-                    div = element.parentNode
-                    list.removeChild(div)
-                } catch (error) {
-                    trash = []
-                }
-                
-            })
-        });
+        deletebtn.addEventListener("click",()=>{
+            div = deletebtn.parentNode
+            list.removeChild(div)
+        }) 
+        
     }  
 }
 
-
-function deleteAction(){
-    console.log("voy a borrar")
-}
-
+ 
